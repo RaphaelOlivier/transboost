cc9cf583e1104e35643766e29a93448123859210
RaphaelOlivier <raphael.olivier@polytechnique.edu>
2017-07-18 16:08:48 +0200

    comment the tool subdir move datasets.py to the series subdir rename testseries.py and datasets.py

--- TransBoost2/tools/display.py
+++ TransBoost2/tools/display.py
@@ -9,18 +9,16 @@ Display functions used to write log files and print things
 
 from __future__ import print_function, absolute_import
 
-def display(*args):
-    logFile=args[0]
-    if(logFile==None):
-        for s in args[1:]:
-            print(s,end='')
-    else:
-        for s in args[1:]:
-            print(s,end='')
-            logFile.write(str(s))
-            
 def displayDict(title=None,dic={},logFile=None):
+    """
+    Prints the content of a dictionnary in a file, or in the shell by default
     
+    Parameters
+    ----------
+    title : String | None (title of the file)
+    dic : dict
+    logFile : String (path of the file to write in, not erased if it exists) | None (no file, print in shell)
+    """
     str0="----------------------------------------------------------------------------\n"
     lstr=[str0,title,"\n",str0]
     for p in dic:
@@ -33,11 +31,19 @@ def displayDict(title=None,dic={},logFile=None):
     else:
         f=open(logFile,'a')
         for s in lstr:
-            print(s,end='')
             f.write(str(s))
         f.close()
                     
 def displayList(title=None,lis=[],logFile=None):
+    """
+    Prints the content of a list in a file, or in the shell by default
+    
+    Parameters
+    ----------
+    title : String | None (title of the file)
+    lis : list
+    logFile : String (path of the file to write in, not erased if it exists) | None (no file, print in shell)
+    """
     
     str0="----------------------------------------------------------------------------\n"
     lstr=[str0,title,"\n",str0]
@@ -50,12 +56,5 @@ def displayList(title=None,lis=[],logFile=None):
     else:
         f=open(logFile,'a')
         for s in lstr:
-#            print(s,end='')
             f.write(str(s)+"\n")
         f.close()
-        
-def invDictKeys(dic):
-    invdic={}
-    for k in dic:
-        invdic[k]=dic[k]
-    return invdic
